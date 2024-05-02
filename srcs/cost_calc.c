edb61d97aa0cc962f37f2b445faf4ea8b6d77bbd
Valerii Bezhevets Composer <145377474+Vbezhevets@users.noreply.github.com>
2024-05-02 12:03:56 +0200

    explain

--- srcs/cost_calc.c
+++ srcs/cost_calc.c
@@ -12,14 +12,14 @@
 
 #include "push_swap.h"
 
-int	steps_s(t_node *s, int size, t_data *_)
+int	steps_s(t_node *s, int size, t_data *_) //calculating steps to top
 {
 	int	steps;
 
 	if (m(s, size, _))
-		steps = size - s->index + 1;
+		steps = size - s->index + 1;  //if node is above middle of it's stack we should
 	else
-		steps = s->index - 1;
+		steps = s->index - 1; //if node is under middle of it's stack just substract
 	s->steps = steps;
 	return (steps);
 }
@@ -30,10 +30,10 @@ int	steps_calc(t_data *_, t_node *s, int s_size, int bg_size)
 	int	bg_steps;
 	int	steps;
 
-	s_steps = steps_s(s, s_size, _);
-	bg_steps = steps_s(s->pair, bg_size, _);
-	if ((m(s, s_size, _) && m(s->pair, bg_size, _))
-		|| (!m(s, s_size, _) && !m(s->pair, bg_size, _)))
+	s_steps = steps_s(s, s_size, _); //steps needed for Node to come to the top
+	bg_steps = steps_s(s->pair, bg_size, _); //steps needed for node to the top in destiantion stack
+	if ((m(s, s_size, _) && m(s->pair, bg_size, _)) //m - calculates if node is under or above the midle of it's stack
+		|| (!m(s, s_size, _) && !m(s->pair, bg_size, _))) // if they both have the same diraction for moving, we can ignore the smaller distance (cause we can use one operation to turn both)
 	{
 		if (s_steps >= bg_steps)
 			steps = s_steps;
@@ -41,7 +41,7 @@ int	steps_calc(t_data *_, t_node *s, int s_size, int bg_size)
 			steps = bg_steps;
 	}
 	else
-		steps = s_steps + bg_steps;
+		steps = s_steps + bg_steps; // if both nodes has different direciton for moving to the top we must add two 
 	return (steps);
 }
 
@@ -53,13 +53,13 @@ t_node	*cost_calc(t_node *s, t_node *bg, int s_size, int bg_size)
 
 	i = 1;
 	min = MAX;
-	while (i <= s_size)
+	while (i <= s_size) // searching for Node that has less steps (qty of operrations)
 	{
 		pair(s, bg, bg_size, 0);
-		s->steps = steps_calc(s->_, s, s_size, bg_size);
+		s->steps = steps_calc(s->_, s, s_size, bg_size); 
 		if (s->steps == 0)
-			return (s);
-		if (s->steps < min)
+			return (s); // allready the best case, no operations needed (excepting push)
+		if (s->steps < min) // less than previous saved in min
 		{
 			min = s->steps;
 			best_s = s;
@@ -68,5 +68,5 @@ t_node	*cost_calc(t_node *s, t_node *bg, int s_size, int bg_size)
 			s = s->next;
 		i++;
 	}
-	return (best_s);
+	return (best_s); // return node that has minimum operations with it's pair
 }
--- srcs/pair.c
+++ srcs/pair.c
@@ -12,7 +12,7 @@
 
 #include "push_swap.h"
 
-void	pair_max(t_node *s, t_node *bg, int size)
+void	pair_max(t_node *s, t_node *bg, int size) // "s" is the Node is a node from A We are searching for minimum in B-stack (before a_to_b) "bg" here is stack B
 {
 	long	max;
 	int		i;
@@ -21,17 +21,17 @@ void	pair_max(t_node *s, t_node *bg, int size)
 	max = MIN;
 	while (i <= size)
 	{
-		if (bg->num > max)
+		if (bg->num > max) // if this number is more than we have before let's remember it:
 		{
 			max = bg->num;
-			s->pair = bg;
+			s->pair = bg; //finaly writing our best pair in this moment inside the "pair"-field in the A Node
 		}
 		bg = bg->next;
 		i++;
 	}
 }
 
-void	pair_min(t_node *s, t_node *bg, int size)
+void	pair_min(t_node *s, t_node *bg, int size) // "s" is the Node is a node from B We are searching for minimum in A-stack (before b_to_a) "bg" here is stack A
 {
 	long	min;
 	int		i;
@@ -40,62 +40,67 @@ void	pair_min(t_node *s, t_node *bg, int size)
 	min = MAX;
 	while (i <= size)
 	{
-		if (bg->num < min)
+		if (bg->num < min) // if this number is less than we have before let's remember it:
 		{
 			min = bg->num;
-			s->pair = bg;
+			s->pair = bg; //finaly writing our best pair in this moment inside the "pair"-field in the B Node
 		}
 		bg = bg->next;
 		i++;
 	}
 }
 
-void	pair_min_diff(t_node *s, t_node *bg, int size)
+void	pair_min_diff(t_node *s, t_node *bg, int size) // writing adress of node with closest number inside pair-field of our Node
 {
 	int		i;
 	long	min_diff;
 
 	min_diff = MAXDIST;
 	i = 0;
-	while (++i <= size)
+	while (++i <= size) // searching in distance recordings in stack of nodes
 	{
-		if (bg->p_found == 1)
+		if (bg->p_found == 1) // it makes sense to look in this node
 		{
-			if (min_diff > bg->dist)
+			if (min_diff > bg->dist) // is the distance written in this node less than we allready found 
 			{
-				min_diff = bg->dist;
-				s->pair = bg;
+				min_diff = bg->dist; // if new distance lower - we remeber it
+				s->pair = bg; // writing our best pair in this moment inside the "pair"-field in the Node
 			}
 		}
-		bg = bg->next;
+		bg = bg->next; // go check next node
 	}
 }
 
-void	pair(t_node *s, t_node *bg, int size, int i)
+void	pair(t_node *s, t_node *bg, int bg_size, int i) // I run this function 2 times : first before moving a_to_b and second before moving b_to_a that's why I have stupid  s->s == 1 and s->s == 2
 {
 	t_node	*bg_start;
 
-	bg_start = bg;
+	bg_start = bg; // start of stack in which we are searching for pair for our Node
 	s->p_found = 0;
-	while (++i <= size)
+	while (++i <= bg_size)
 	{
-		if ((s->num > bg->num && s->s == 1) || (s->num < bg->num && s->s == 2))
-		{
-			if (s->s == 1)
-				bg->dist = s->num - bg->num;
-			else
-				bg->dist = bg->num - s->num;
+//s->s == 1 means we searching for pair to A in B stack ("bg" is B stack now, "s" is a Node from A-stack)
+//s->s == 2 means we searching for pair to B in A stack ("bg" is A stack now, "s" is a Node from B-stack)
+		if ((s->num > bg->num && s->s == 1) || (s->num < bg->num && s->s == 2)) 
+ 		{
+			if (s->s == 1)// (before  a_to_b)
+				bg->dist = s->num - bg->num; // writing in node from B stack found distance with number: A-stack->node->num minus B-stack->node->num
+			else // before (b_to_a)
+				bg->dist = bg->num - s->num; // writing in node from A stack found distance with number: B-stack->node->num minus A-stack->node->num
 			bg->p_found = 1;
 			s->p_found = 1;
 		}
 		else
-			bg->p_found = -1;
+			bg->p_found = -1; // wasn't find lower pair for A Node (when a_to_b) or higher pair for B Node (when b_to_a)
 		bg = bg->next;
 	}
-	if (s->p_found != 1 && s->s == 1)
-		pair_max(s, bg_start, size);
-	else if (s->p_found != 1 && s->s == 2)
-		pair_min(s, bg_start, size);
+	if (s->p_found != 1 && s->s == 1) // this case when smaller was not found and "s" is the Node from A-stack ( a_to_b)
+		pair_max(s, bg_start, bg_size);  
+	else if (s->p_found != 1 && s->s == 2) // this case when smaller was not found ("s" is the Node from B-stack ( b_to_a)
+		pair_min(s, bg_start, bg_size); 
 	else
-		pair_min_diff(s, bg_start, size);
+		pair_min_diff(s, bg_start, bg_size); // search for a pair with the minimum distance (closest number) from those found
 }
+
+//Node ("s") - is our particular node for which we are looking for a pair
+//bg - is Stack the stack we are iterating over
